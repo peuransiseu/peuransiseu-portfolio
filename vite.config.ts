d0fd42a5487858c9a5585ec3308452f94f999af1
peuransiseu <90952718+peuransiseu@users.noreply.github.com>
2026-04-05 10:01:43 +0800

    Update base URL for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/https://peuransiseu.github.io/',
+  base: '/peuransiseu-portfolio/',
   build: {
     rollupOptions: {
       input: {
